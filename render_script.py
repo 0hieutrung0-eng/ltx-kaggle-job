5a508c7bce9aa8173c9fb581b3b29bcbf752f0fa
0hieutrung0-eng <0hieutrung0@gmail.com>
2026-09-21 15:34:58 +0700

    Update render_script.py

--- render_script.py
+++ render_script.py
@@ -306,9 +306,9 @@ async def process_video_pipeline():
                 image_input,
                 decode_chunk_size=8,
                 generator=generator,
-                num_frames=25,
-                motion_bucket_id=127,
-                noise_aug_strength=0.02,
+                num_frames=20,
+                motion_bucket_id=75,
+                noise_aug_strength=0.01,
             ).frames[0]
 
             export_to_video(frames, raw_video_file, fps=7)
